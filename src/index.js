d8bb328370138912aa91c2bbc280ef3384fe5a57
Marsel Sharipov <sharipovmarsel@gmail.com>
2025-06-30 23:53:37 +0500

    Включён сервис-воркер и автообновление PWA

--- src/index.js
+++ src/index.js
@@ -15,7 +15,18 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register({
+  onUpdate: registration => {
+    if (registration && registration.waiting) {
+      registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+      registration.waiting.addEventListener('statechange', e => {
+        if (e.target.state === 'activated') {
+          window.location.reload();
+        }
+      });
+    }
+  }
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
