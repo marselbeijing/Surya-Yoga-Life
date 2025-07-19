571f9a86b3fdb5a250e7a68f676b9855429aab01
Marsel Sharipov <sharipovmarsel@gmail.com>
2025-07-19 17:19:29 +0500

    Иконки увеличены ещё на 5% (с 77px до 81px)

--- src/components/MainSections.js
+++ src/components/MainSections.js
@@ -5,37 +5,37 @@ const sections = [
   {
     key: 'Медитация',
     title: 'Медитация',
-    icon: <img src={process.env.PUBLIC_URL + '/medi.svg'} alt="Медитация" style={{ width: 77, height: 77, display: 'block', margin: '0 auto' }} />,
+    icon: <img src={process.env.PUBLIC_URL + '/medi.svg'} alt="Медитация" style={{ width: 81, height: 81, display: 'block', margin: '0 auto' }} />,
   },
   {
     key: 'Йога',
-    icon: <img src={process.env.PUBLIC_URL + '/meditatsiya.svg'} alt="Йога" style={{ width: 77, height: 77, display: 'block', margin: '0 auto' }} />,
+    icon: <img src={process.env.PUBLIC_URL + '/meditatsiya.svg'} alt="Йога" style={{ width: 81, height: 81, display: 'block', margin: '0 auto' }} />,
     title: 'Йога',
   },
   {
     key: 'Дыхание',
     title: 'Дыхание',
-    icon: <img src={process.env.PUBLIC_URL + '/lung.svg'} alt="Дыхание" style={{ width: 77, height: 77, display: 'block', margin: '0 auto' }} />,
+    icon: <img src={process.env.PUBLIC_URL + '/lung.svg'} alt="Дыхание" style={{ width: 81, height: 81, display: 'block', margin: '0 auto' }} />,
   },
   {
     key: 'Лила',
     title: 'Лила',
-    icon: <img src={process.env.PUBLIC_URL + '/card.svg'} alt="Лила" style={{ width: 77, height: 77, display: 'block', margin: '0 auto' }} />,
+    icon: <img src={process.env.PUBLIC_URL + '/card.svg'} alt="Лила" style={{ width: 81, height: 81, display: 'block', margin: '0 auto' }} />,
   },
   {
     key: 'Шанкапракшалана',
     title: 'Шанкапракшалана',
-    icon: <img src={process.env.PUBLIC_URL + '/water1.svg'} alt="Шанкапракшалана" style={{ width: 77, height: 77, display: 'block', margin: '0 auto' }} />,
+    icon: <img src={process.env.PUBLIC_URL + '/water1.svg'} alt="Шанкапракшалана" style={{ width: 81, height: 81, display: 'block', margin: '0 auto' }} />,
   },
   {
     key: 'Дневник Жизни',
     title: 'Дневник Жизни',
-    icon: <img src={process.env.PUBLIC_URL + '/diary1.svg'} alt="Дневник Жизни" style={{ width: 77, height: 77, display: 'block', margin: '0 auto' }} />,
+    icon: <img src={process.env.PUBLIC_URL + '/diary1.svg'} alt="Дневник Жизни" style={{ width: 81, height: 81, display: 'block', margin: '0 auto' }} />,
   },
   {
     key: 'Гвоздестояние',
     title: 'Гвоздестояние',
-    icon: <img src={process.env.PUBLIC_URL + '/foot.svg'} alt="Гвоздестояние" style={{ width: 77, height: 77, display: 'block', margin: '0 auto' }} />,
+    icon: <img src={process.env.PUBLIC_URL + '/foot.svg'} alt="Гвоздестояние" style={{ width: 81, height: 81, display: 'block', margin: '0 auto' }} />,
   },
 ];
 
