bb7c5848a84ccfb784950b3a53c945a4e890887b
Marsel Sharipov <sharipovmarsel@gmail.com>
2025-07-21 19:24:34 +0500

    Фикс: одинаковое отображение приложения на телефоне и в браузере (мобильный фрейм, корректные размеры)

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -1,41 +1,36 @@
 import React from 'react';
 import './BottomBar.css';
-import KnowledgeIcon from './knowledge.svg';
 import HomeIcon from './home.svg';
+import KnowledgeIcon from './knowledge.svg';
 import AboutIcon from './about.svg';
 import AccountIcon from './account.svg';
 
-const tabs = [
-  {
-    key: 'home',
-    label: 'Главная',
-    icon: <img src={HomeIcon} alt="Главная" />,
-  },
-  {
-    key: 'knowledge',
-    label: 'База знаний',
-    icon: <img src={KnowledgeIcon} alt="База знаний" />,
-  },
-  {
-    key: 'about',
-    label: 'О нас',
-    icon: <img src={AboutIcon} alt="О нас" />,
-  },
-  {
-    key: 'account',
-    label: 'Аккаунт',
-    icon: <img src={AccountIcon} alt="Аккаунт" />,
-  },
-];
+const BottomBar = ({ activeTab, onTabChange }) => {
+  const tabs = [
+    {
+      key: 'home',
+      label: 'Главная',
+      icon: <img src={HomeIcon} alt="Главная" />,
+    },
+    {
+      key: 'about',
+      label: 'О нас',
+      icon: <img src={AboutIcon} alt="О нас" />,
+    },
+    {
+      key: 'account',
+      label: 'Аккаунт',
+      icon: <img src={AccountIcon} alt="Аккаунт" />,
+    },
+  ];
 
-export default function BottomBar({ activeTab, setActiveTab }) {
   return (
     <nav className="bottom-bar">
       {tabs.map((tab) => (
         <div
           className={`bar-tab${activeTab === tab.key ? ' active' : ''}`}
           key={tab.key}
-          onClick={() => setActiveTab(tab.key)}
+          onClick={() => onTabChange(tab.key)}
         >
           <span className="bar-icon">{tab.icon}</span>
           <span className="bar-title">{tab.label}</span>
@@ -43,4 +38,6 @@ export default function BottomBar({ activeTab, setActiveTab }) {
       ))}
     </nav>
   );
-} 
+};
+
+export default BottomBar; 
